21237a94aa744ed620d4727a50d8bfc7175fbceb
Andres <andres.gonzalez2392@gmail.com>
2022-08-25 15:59:51 -0400

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by PDWS on 8/25/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
